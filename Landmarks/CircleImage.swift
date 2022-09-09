f6f836783fd7ec43cbfa28d73f832934f285099c
Timp Yorke <thonghitak.tim@gmail.com>
2022-09-09 11:42:12 +0700

    Chapter.1 : Creating and Combining Views

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Thongphitak Sowanna on 9/9/2565 BE.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.gray, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
