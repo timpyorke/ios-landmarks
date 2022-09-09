c26faa0b4b916034840437e67a5ff0197749d8e1
Timp Yorke <thonghitak.tim@gmail.com>
2022-09-09 11:25:12 +0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Thongphitak Sowanna on 9/9/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
